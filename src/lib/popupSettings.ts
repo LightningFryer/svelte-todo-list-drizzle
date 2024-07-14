5adaba4d869c1ac6f5159083d187fd29562ece70
LightningFryer <91123163+LightningFryer@users.noreply.github.com>
2024-07-14 15:13:12 +0530

    Nuked main branch files

--- src/lib/popupSettings.ts
+++
@@ -1,7 +0,0 @@
-import type { PopupSettings } from "@skeletonlabs/skeleton";
-
-export const popupUserIconHover: PopupSettings = {
-    event: "hover",
-    target: "popupUserIconHover",
-    placement: "bottom"
-}
--- src/zodSchema.ts
+++
@@ -1,6 +0,0 @@
-import { z } from "zod"
-
-export const newTodoSchema = z.object({
-    title: z.string({required_error: "Title is required!"}).min(1),
-    content: z.string({required_error: "Content is required!"}).min(1)
-});
